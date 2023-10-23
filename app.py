1a2e4016ab392ec325e17010f71a77a66a9114cb
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-23 02:16:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def setOpenAIKey(key):
 
 
 def get_text(n):
-    input_text = st.text_input('How can I help?', '', key="input{}".format(n))
+    input_text = st.text_input(', '', key="input{}".format(n))
     return input_text
 
 
@@ -46,8 +46,8 @@ def main():
         tabs = st.tabs([s.center(9, "\u2001") for s in st.session_state["tabs"]])
         show_data(tabs, selected_df)
 
-    st.header("AI Agent Output Directory")
-    if st.button('Open Directory'):
+    st.header("")
+    if st.button('Refresh visuals'):
         current_dir = os.getcwd()
         if platform.system() == "Darwin":  # macOS
             subprocess.Popen(["open", current_dir])
@@ -60,10 +60,10 @@ def main():
     imgs_jpeeg = glob.glob('*.jpeg')
     imgs_ = imgs_png + imgs_jpg + imgs_jpeeg
     if len(imgs_) > 0:
-        img = image_select("Generated Charts/Graphs", imgs_, captions=imgs_, return_value='index')
+        img = image_select("", imgs_, captions=imgs_, return_value='index')
         st.write(img)
 
-    st.header("Query The Dataframes")
+    st.header("")
     x = 0
     user_input = get_text(x)
     if st.button('Query'):
