357c68e2fd46140bef2f5cbd0c02a797f0abd985
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-26 07:33:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -70,17 +70,6 @@ def main():
     username = st.text_input('', placeholder='Username')
     
 
-    if st.button('Load visuals'):
-        print("Directory opened:", current_dir)
-    imgs_png = glob.glob('*.png')
-    imgs_jpg = glob.glob('*.jpg')
-    imgs_jpeeg = glob.glob('*.jpeg')
-    imgs_ = imgs_png + imgs_jpg + imgs_jpeeg
-    if len(imgs_) > 0:
-        img = image_select("", imgs_, captions=imgs_, return_value='index')
-        st.write(img)    
-    
-
 
     query_counts = load_query_counts()
 
@@ -111,6 +100,16 @@ def main():
             st.error('Daily query limit (25) reached for this user.')
     else:
         st.error('User not found. Please check your username.')
+
+    if st.button('Load visuals'):
+        print("Directory opened:", current_dir)
+    imgs_png = glob.glob('*.png')
+    imgs_jpg = glob.glob('*.jpg')
+    imgs_jpeeg = glob.glob('*.jpeg')
+    imgs_ = imgs_png + imgs_jpg + imgs_jpeeg
+    if len(imgs_) > 0:
+        img = image_select("", imgs_, captions=imgs_, return_value='index')
+        st.write(img)    
 
 if __name__ == "__main__":
     if 'generated' not in st.session_state:
