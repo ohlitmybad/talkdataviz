e31439ffeb8cf607283dca2803f789be22b5441a
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-26 07:49:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,10 +77,17 @@ def main():
             subprocess.Popen(["explorer", current_dir])
         else:
             print("Directory opened:", current_dir)
+    st.header("Image Gallery")
     imgs_png = glob.glob('*.png')
     imgs_jpg = glob.glob('*.jpg')
     imgs_jpeeg = glob.glob('*.jpeg')
     imgs_ = imgs_png + imgs_jpg + imgs_jpeeg
+    
+    if len(imgs_) > 0:
+        img = image_select("", imgs_, captions=imgs_, return_value='index')
+        st.write(img)
+    else:
+        st.info("No images available in the gallery.")
 
     selected_images = st.multiselect("Select images to remove", imgs_)
     if st.button("Remove Selected"):
