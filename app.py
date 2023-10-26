633909d16574cefdb6f64ba89f5c133cd8b83956
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-26 07:29:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def main():
     imgs_jpeeg = glob.glob('*.jpeg')
     imgs_ = imgs_png + imgs_jpg + imgs_jpeeg
     if len(imgs_) > 0:
-        img = image_select("", imgs_, captions=imgs_, return_value='index')
+        img = image_select(imgs_, captions=imgs_, return_value='index')
         st.write(img)    
     
 
