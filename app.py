9227851142017c8879b7fb79da8bb44002fc6a34
ohlitmybad <130418672+ohlitmybad@users.noreply.github.com>
2023-10-26 07:17:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,6 +66,23 @@ def main():
     setOpenAIKey()
     agent, selected_df, selected_df_names = save_uploaded_file()
 
+    st.header("")
+    if st.button('📊'):
+        current_dir = os.getcwd()
+        if platform.system() == "Darwin":  # macOS
+            subprocess.Popen(["open", current_dir])
+        elif platform.system() == "Windows":
+            subprocess.Popen(["explorer", current_dir])
+        else:
+            print("Directory opened:", current_dir)
+    imgs_png = glob.glob('*.png')
+    imgs_jpg = glob.glob('*.jpg')
+    imgs_jpeeg = glob.glob('*.jpeg')
+    imgs_ = imgs_png + imgs_jpg + imgs_jpeeg
+    if len(imgs_) > 0:
+        img = image_select("", imgs_, captions=imgs_, return_value='index')
+        st.write(img)    
+    
     # User login
     username = st.text_input('Username:')
 
